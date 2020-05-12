04c3413905b84ad0ab8574797c1f20119bb41eef
answerDong <gaoerdongchuang@163.com>
2020-05-12 17:52:16 +0800

    Create UserListViewModel.kt

---
+++ app/src/main/java/com/bcd/wanandroid/mvvmdemo/UserListViewModel.kt
@@ -0,0 +1,9 @@
+package com.bcd.wanandroid.mvvmdemo
+
+/**
+ * @author D.chuang
+ * @date 2020/5/11.
+ * gitee：https://gitee.com/gaoerdongchuang/fycz_new
+ * email：gaoerdongchuang@163.com
+ * description：页面
+ */
