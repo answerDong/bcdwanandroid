12acc5587b2d7402963c515398653b875b462aa4
answerDong <gaoerdongchuang@163.com>
2020-05-22 18:07:03 +0800

    1.协程

---
+++ app/src/main/java/com/bcd/wanandroid/coroutines/CoroutinesActivity.kt
@@ -0,0 +1,14 @@
+package com.bcd.wanandroid.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author D.chuang
+ * @date 2020/5/22.
+ * gitee：https://gitee.com/gaoerdongchuang/fycz_new
+ * email：gaoerdongchuang@163.com
+ * description：页面
+ */
+class CoroutinesActivity :AppCompatActivity,CoroutineScope by MainSc{
+
+}
